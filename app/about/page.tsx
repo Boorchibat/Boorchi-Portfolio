2201781810f3cde45a1b01598c56d15aeaca18be
Boorchibat <coolboyboorchi@gmail.com>
2026-06-12 03:36:44 -0400

    me image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Skills } from "../components/aboutComponents/Skills";
 import { TimeLine } from "../components/aboutComponents/TimeLine";
+import Image from "next/image";
 
 type DataType = {
   date: string;
@@ -12,22 +13,26 @@ const Data: DataType[] = [
   {
     date: "2023 - 2024",
     title: "Learning code for the first time at Pinecone academy",
-    description: "I spent the summer of my freshman year of high school learning how to code at pinecone academy where I learned how to code basic HTML, CSS and Javascript. This is where I started to make some of my small projects for fun after my classes.",
+    description:
+      "I spent the summer of my freshman year of high school learning how to code at pinecone academy where I learned how to code basic HTML, CSS and Javascript. This is where I started to make some of my small projects for fun after my classes.",
   },
   {
     date: "2024 - 2025",
     title: "Entering the HOP program and learning full stack development",
-    description: "After the summer I spent at Pinecone summer academy, I was interested in learning more about coding and wanted to go more advanced. This is where I was introduced to ReactJS, NextJS, nodeJS, mongoDB and more tools on full stack developing over the course of my sophmore school year.",
+    description:
+      "After the summer I spent at Pinecone summer academy, I was interested in learning more about coding and wanted to go more advanced. This is where I was introduced to ReactJS, NextJS, nodeJS, mongoDB and more tools on full stack developing over the course of my sophmore school year.",
   },
   {
     date: "Summer 2025",
     title: "Interning as a full stack developer at Pinecone",
-    description: "After finishing my time in the HOP program, I applied and was offered a spot to intern at pinecone over the summer and worked in a team of 6 other interns to create a real estate website. Additionally, I worked in person in Mongolia as an assistant teacher for Pinecone academy and taught 30 students how to code HTML, CSS and Javascript.",
+    description:
+      "After finishing my time in the HOP program, I applied and was offered a spot to intern at pinecone over the summer and worked in a team of 6 other interns to create a real estate website. Additionally, I worked in person in Mongolia as an assistant teacher for Pinecone academy and taught 30 students how to code HTML, CSS and Javascript.",
   },
   {
     date: "2025-2026",
     title: "Independent Full Stack Developer in Junior year of high school",
-    description: "After finishing my internship at pinecone, I focused on school for most of the year and made occasional projects, such as a Lost and found website I made for my school FBLA competition. I also started to plan and design future projects that I am planning to work on in Mongolia.",
+    description:
+      "After finishing my internship at pinecone, I focused on school for most of the year and made occasional projects, such as a Lost and found website I made for my school FBLA competition. I also started to plan and design future projects that I am planning to work on in Mongolia.",
   },
 ];
 
@@ -44,10 +49,16 @@ const page = () => {
       </h1>
 
       <div className="w-full flex flex-col md:flex-row justify-between gap-6 mt-[30px]">
-        <div className="w-full md:w-[40%] bg-gray-800 rounded-md border border-gray-500 p-8">
-          <h1 className="text-[20px]">Who I am</h1>
-          <p>wassup</p>
+        <div className="w-full md:w-[60%] flex justify-center items-center">
+          <Image
+            src={"/me.jpeg"}
+            alt="Boorchi Image"
+            className=" rounded-lg"
+            width={500}
+            height={500}
+          />
         </div>
+
         <div className="w-full md:w-[40%]">
           <Skills />
         </div>
